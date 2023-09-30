af4b43aee7d6c39f7526944f4eb7e1a7babb3efe
corbelli.s98 <corbelli.s98@gmail.com>
2023-09-30 21:23:37 +0200

    Rimosso campo inutile

--- ApiService/ApiService/Service/Order/Dto/OrderDto.cs
+++ ApiService/ApiService/Service/Order/Dto/OrderDto.cs
@@ -2,7 +2,6 @@
 {
     public class OrderDto
     {
-        public int OrderNumber { get; set; }
         public string OrderName { get; set; }
         public List<int> ProductIds { get; set; }
     }
